b261f6fd3c119537adc107c46173297ee78b8c55
최수환 <frcp9408@gmail.com>
2023-09-02 16:33:45 +0900

    0902_최수환_16:33_몽고db 중간저장

--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const connect = require("./schemas");
+
+connect();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/board", require("./routes/boardRouter"));
 
 app.listen(8088, () => {
     console.log("8088 진입");
