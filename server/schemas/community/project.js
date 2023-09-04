b29958075ba86a6457cd6d8938b8c7f0b8bc0fbf
KwangYoungHeo <sslgo0214@gmail.com>
2023-09-04 20:30:33 +0900

    0904_20:29_허광영_community스키마,라우터폴더 생성, 각 게시판 스키마 작성

---
+++ server/schemas/community/project.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const projectSchema = new Schema({
+
+    // 게시판마다 반복되는 부분
+    writer: {
+        // 작성자
+        type: String,
+        required: true,
+        // ref: "User"
+    },
+    title: {
+        // 제목
+        type: String,
+        required: true
+    },
+    content: {
+        // 내용
+        type: String,
+        required: true
+    },
+    postNum: {
+        // 글넘버
+        type: Number
+    },
+    createdAt: {
+        // 작성시간
+        type: Date,
+        default: Date.now
+    },
+    views: {
+        // 조회수
+        type: Number
+    },
+
+    // 게시판마다 달라지는 부분
+    itemCatogory: {
+        // 글카테고리
+        type: String,
+        // required : true
+    },
+    imgPath: {
+        // 이미지 주소
+        type: String
+    },
+    recruitPeriod: {
+        // 모집기간
+        type: Date
+    },
+    recruit: {
+        // 모집인원
+        type: Number
+    }
+})
+
+module.exports = mongoose.model('Project', projectSchema, 'Project')
