fdac1709d332848956785937a9198bf719624df2
BaikJinHyeock <me335097@gmail.com>
2023-09-04 20:43:22 +0900

    0904_20:43_백진혁_폰트설정 및 해더수정

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./css/reset.css";
 import App from "./App";
 // import App from './DBtestApp';
 import reportWebVitals from "./reportWebVitals";
