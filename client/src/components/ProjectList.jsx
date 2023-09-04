f68047dcb6f93fcf0ce755ad50f1923bcd63f07c
SooHwanC <frcp9408@gmail.com>
2023-09-04 20:13:20 +0900

    0904_20:13_최수환_css 모듈화

--- client/src/components/ProjectList.jsx
+++ client/src/components/ProjectList.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import LeftContainer from "./LeftContainer";
 import { Link } from "react-router-dom";
-import "../css/Main.css";
-import "../css/Community.css";
+// import "../css/Community.css";
+import styles from '../css/Community.module.css'
+
 
 const ProjectList = () => {
   return (
-    <div className="Main_container">
+    <div className={styles.Main_container}>
       <LeftContainer />
 
-      <div className="right_container">
-        <div className="Main_container_banner">banner</div>
+      <div className={styles.right_container}>
+        <div className={styles.Main_container_banner}>banner</div>
         <h2>Communaty</h2>
 
-        <div className="Main_container_list">
+        <div className={styles.Main_container_list}>
           {/* 글 반복 시작 */}
-          <div className="Main_container_list_detail">
+          <div className={styles.Main_container_list_detail}>
             <div>
-              <p className="b_date">1일 전</p>
+              <p className={styles.b_date}>1일 전</p>
               <Link to={"/projectDetail"}>
                 <h4>공공기관 프로젝트 함께 진행할 사람 모집중!</h4>
               </Link>
@@ -28,7 +29,7 @@ const ProjectList = () => {
 
             <div>
               <div>
-                <p className="b_date">데이터 디자인</p>
+                <p className={styles.b_date}>데이터 디자인</p>
                 <h4>수업중 몰래롤</h4>
               </div>
               <img src="#" />
