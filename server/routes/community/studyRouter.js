2bba9b1fe8e3eb6364bf791d01c7225076128a64
KwangYoungHeo <sslgo0214@gmail.com>
2023-09-05 23:10:50 +0900

    0905_23:10_허광영_studyRouter 조금 수정

--- server/routes/community/studyRouter.js
+++ server/routes/community/studyRouter.js
@@ -11,7 +11,17 @@ router.post('/write', async (req, res) => {
         obj = {
             writer: req.body.writer,
             title: req.body.title,
-            content: req.body.content
+            content: req.body.content,
+
+            // 페이지 만들어지면 수정할 것
+            // 글 번호는 어떤식으로 생성할 건지, 이미지 주소는 어떻게 줄일 건지 방법 찾기
+            // postNum: req.body.postNum,
+            // views: 0,
+            
+            // postCategory: req.body.categotry,
+            // imgPath: req.body.imgPath,
+            // recruitPeriod: req.body.recruitPeriod,
+            // recruit: req.body.recruit
         };
         
         const study = new Study(obj);
@@ -21,6 +31,11 @@ router.post('/write', async (req, res) => {
         console.log(err);
         res.json({ message: false });
       }
+});
+
+// 글 수정
+router.post("/update/:_id", async(req, res) =>{
+  
 })
 
 // 밑으로 수정, 삭제 등등 작성할 것
