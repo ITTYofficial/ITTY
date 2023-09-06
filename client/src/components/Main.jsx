17cbdfbca2602e6f3d58984d252781a55dc035ca
BaikJinHyeock <me335097@gmail.com>
2023-09-06 09:01:57 +0900

    0906_0901_백진혁

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -5,6 +5,24 @@ import style from "../css/Main.module.css";
 // import "../css/Community.css";
 
 const Main = () => {
+  const Main_detail = () => (
+    <div className={style.Main_grid_detail}>
+      <h4>핵심끝나고 너무 힘들어요.. 다들 어떠세요</h4>
+      <div>
+        <p>1일전 </p>
+        <p>👁28 </p>
+        <p>💬4</p>
+        <div className={style.Main_grid_profile}>
+          <span className={style.profile_text}>
+            <p>데이터디자인반</p>
+            <h4>자바노잼</h4>
+          </span>
+          <span className={style.profile_pic}>{/* <img src="#" />/ */}</span>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <div className={style.Wrap_container}>
       {/* 메인 이미지슬라이드 시작 */}
@@ -19,93 +37,12 @@ const Main = () => {
             <h3>자유게시판⚽</h3>
 
             {/* 자유게시판 목록 리스트 반복시작 */}
-            <div className={style.Main_grid_detail}>
-              <h4>핵심끝나고 너무 힘들어요.. 다들 어떠세요</h4>
-              <div>
-                <p>1일전 </p>
-                <p>👁28 </p>
-                <p>💬4</p>
-                <div className={style.Main_grid_profile}>
-                  <span className={style.profile_text}>
-                    <p>데이터디자인반</p>
-                    <h4>자바노잼</h4>
-                  </span>
-                  <span className={style.profile_pic}>
-                    {/* <img src="#" />/ */}
-                  </span>
-                </div>
-              </div>
-            </div>
+            <Main_detail />
+            <Main_detail />
+            <Main_detail />
+            <Main_detail />
+            <Main_detail />
             {/* 자유게시판 목록 리스트 반복 끝 */}
-
-            <div className={style.Main_grid_detail}>
-              <h4>핵심끝나고 너무 힘들어요.. 다들 어떠세요</h4>
-              <div>
-                <p>1일전 </p>
-                <p>👁28 </p>
-                <p>💬4</p>
-                <div className={style.Main_grid_profile}>
-                  <span className={style.profile_text}>
-                    <p>데이터디자인반</p>
-                    <h4>자바노잼</h4>
-                  </span>
-                  <span className={style.profile_pic}>
-                    {/* <img src="#" />/ */}
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className={style.Main_grid_detail}>
-              <h4>핵심끝나고 너무 힘들어요.. 다들 어떠세요</h4>
-              <div>
-                <p>1일전 </p>
-                <p>👁28 </p>
-                <p>💬4</p>
-                <div className={style.Main_grid_profile}>
-                  <span className={style.profile_text}>
-                    <p>데이터디자인반</p>
-                    <h4>자바노잼</h4>
-                  </span>
-                  <span className={style.profile_pic}>
-                    {/* <img src="#" />/ */}
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className={style.Main_grid_detail}>
-              <h4>핵심끝나고 너무 힘들어요.. 다들 어떠세요</h4>
-              <div>
-                <p>1일전 </p>
-                <p>👁28 </p>
-                <p>💬4</p>
-                <div className={style.Main_grid_profile}>
-                  <span className={style.profile_text}>
-                    <p>데이터디자인반</p>
-                    <h4>자바노잼</h4>
-                  </span>
-                  <span className={style.profile_pic}>
-                    {/* <img src="#" />/ */}
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className={style.Main_grid_detail}>
-              <h4>핵심끝나고 너무 힘들어요.. 다들 어떠세요</h4>
-              <div>
-                <p>1일전 </p>
-                <p>👁28 </p>
-                <p>💬4</p>
-                <div className={style.Main_grid_profile}>
-                  <span className={style.profile_text}>
-                    <p>데이터디자인반</p>
-                    <h4>자바노잼</h4>
-                  </span>
-                  <span className={style.profile_pic}>
-                    {/* <img src="#" />/ */}
-                  </span>
-                </div>
-              </div>
-            </div>
           </div>
 
           {/* ======오른쪽 메인컨텐츠 왼쪽 오른쪽 구분선====== */}
@@ -114,75 +51,8 @@ const Main = () => {
             <h3>프로젝트/스터디 구해요🙋‍♂️</h3>
 
             {/* 프로젝트 / 스터디 목록 리스트 반복시작 */}
-            <div className={style.Main_grid_detail}>
-              <h4>공공기관 프로젝트 같이 하실 분</h4>
-              <div>
-                <p>1일전</p>
-                <p>👁 14</p>
-                <p>💬 8</p>
-                <div className={style.Main_grid_profile}>
-                  <span className={style.profile_text}>
-                    <p>빅데이터분석반</p>
-                    <h4>비전공자</h4>
-                  </span>
-                  <span className={style.profile_pic}>
-                    {/* <img src="#" />/ */}
-                  </span>
-                </div>
-              </div>
-            </div>
+            <Main_detail />
             {/* 프로젝트 / 스터디 목록 리스트 끝 */}
-            <div className={style.Main_grid_detail}>
-              <h4>공공기관 프로젝트 같이 하실 분</h4>
-              <div>
-                <p>1일전</p>
-                <p>👁 14</p>
-                <p>💬 8</p>
-                <div className={style.Main_grid_profile}>
-                  <span className={style.profile_text}>
-                    <p>빅데이터분석반</p>
-                    <h4>비전공자</h4>
-                  </span>
-                  <span className={style.profile_pic}>
-                    {/* <img src="#" />/ */}
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className={style.Main_grid_detail}>
-              <h4>공공기관 프로젝트 같이 하실 분</h4>
-              <div>
-                <p>1일전</p>
-                <p>👁 14</p>
-                <p>💬 8</p>
-                <div className={style.Main_grid_profile}>
-                  <span className={style.profile_text}>
-                    <p>빅데이터분석반</p>
-                    <h4>비전공자</h4>
-                  </span>
-                  <span className={style.profile_pic}>
-                    {/* <img src="#" />/ */}
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className={style.Main_grid_detail}>
-              <h4>공공기관 프로젝트 같이 하실 분</h4>
-              <div>
-                <p>1일전</p>
-                <p>👁 14</p>
-                <p>💬 8</p>
-                <div className={style.Main_grid_profile}>
-                  <span className={style.profile_text}>
-                    <p>빅데이터분석반</p>
-                    <h4>비전공자</h4>
-                  </span>
-                  <span className={style.profile_pic}>
-                    {/* <img src="#" />/ */}
-                  </span>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
