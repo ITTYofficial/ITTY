838705220fa0aeb4fb05307a1b2d39e3bca7b5e0
최수환 <frcp9408@gmail.com>
2023-09-06 23:10:11 +0900

    0906_23:09_최수환_toast API 임시커밋

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import MarketList from "./components/MarketList";
 import MarketDetail from "./components/MarketDetail";
 import Join from "./components/Join";
 import Login from "./components/Login";
+import ToastTest from "./components/ToastTest";
 
 
 function App() {
@@ -23,6 +24,8 @@ function App() {
         <Route path="/marketDetail" element={<MarketDetail />}></Route>
         <Route path="/join" element={<Join />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/toast" element={<ToastTest />}></Route>
+        
       </Routes>
       <Footer />
     </div>
