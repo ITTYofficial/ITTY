3d489a73442e18e187619a3cee1484670eaaeb33
최수환 <frcp9408@gmail.com>
2023-09-12 19:26:01 +0900

    0912_19:25_최수환_마켓디테일 껍데기

---
+++ client/src/components/TestPage.jsx
@@ -0,0 +1,22 @@
+
+
+import Image from 'react-bootstrap/Image';
+
+
+function TestPage() {
+    return (
+        <div>
+
+
+
+            
+                <Image src="https://i1.ruliweb.com/img/22/07/28/18242f82cc7547de2.png" roundedCircle />
+            
+
+
+
+        </div>
+    );
+}
+
+export default TestPage;
