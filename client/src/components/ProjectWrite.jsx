838921271e17605f191085bf0e32e48b069c5a47
KwangYoungHeo <sslgo0214@gmail.com>
2023-09-13 20:34:47 +0900

    0913_20:34_허광영_프로젝트게시글 작성, 조회 구현

--- client/src/components/ProjectWrite.jsx
+++ client/src/components/ProjectWrite.jsx
@@ -1,25 +1,28 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import style from "../css/ProjectWrite.module.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import QuillTest from './QuillTest';
+import axios from 'axios';
+import { PlayBoardContext } from '../context/PlayBoardContext';
 
 
 
 
 const ProjectWrite = () => {
 
-    const [selectedValues, setSelectedValues] = useState([]);
+    const [position, setposition] = useState([]);
+    const { value } = useContext(PlayBoardContext);
 
     // 포지션 함수
     function changeColor(value) {
-        if (selectedValues.includes(value)) {
+        if (position.includes(value)) {
             // 이미 선택된 버튼인 경우 선택 해제
-            setSelectedValues(selectedValues.filter(item => item !== value));
+            setposition(position.filter(item => item !== value));
         } else {
             // 누른 버튼 값 추가
-            setSelectedValues([...selectedValues, value]);
+            setposition([...position, value]);
         }
     }
 
@@ -32,14 +35,29 @@ const ProjectWrite = () => {
         event.preventDefault();
 
         // 작성된 값 확인할수 있는곳
-        console.log('선택한 포지션:', selectedValues);
+        console.log('선택한 포지션:', position);
         console.log('프로젝트 시작일:', startDate);
         console.log('프로젝트 종료일:', endDate);
         const formData = new FormData(event.target);
 
+        const obj = {};
         formData.forEach((value, key) => {
             console.log(`폼 요소 이름: ${key}, 값: ${value}`);
+            obj[key] = value;
         });
+        obj['startDate'] = startDate;
+        obj['endDate'] = endDate;
+        obj['content'] = value;
+        console.log(obj);
+
+        axios.post('http://localhost:8088/project/write', obj)
+        .then((res)=>{
+            console.log("통신 완료");
+        })
+        .catch((err)=>{
+            console.log("통신 실패");
+            console.log(err);
+        })
     }
 
     return (
@@ -54,40 +72,40 @@ const ProjectWrite = () => {
                 <button
                     type="button"
                     onClick={() => changeColor('1')}
-                    style={{ backgroundColor: selectedValues.includes('1') ? '#ABE9FF' : '' }}
+                    style={{ backgroundColor: position.includes('1') ? '#ABE9FF' : '' }}
                 >
                     백엔드
                 </button>
                 <button
                     type="button"
                     onClick={() => changeColor('2')}
-                    style={{ backgroundColor: selectedValues.includes('2') ? '#ABE9FF' : '' }}
+                    style={{ backgroundColor: position.includes('2') ? '#ABE9FF' : '' }}
                 >
                     프론트엔드
                 </button>
                 <button
                     type="button"
                     onClick={() => changeColor('3')}
-                    style={{ backgroundColor: selectedValues.includes('3') ? '#ABE9FF' : '' }}
+                    style={{ backgroundColor: position.includes('3') ? '#ABE9FF' : '' }}
                 >
                     풀스택
                 </button>
                 <button
                     type="button"
                     onClick={() => changeColor('4')}
-                    style={{ backgroundColor: selectedValues.includes('4') ? '#ABE9FF' : '' }}
+                    style={{ backgroundColor: position.includes('4') ? '#ABE9FF' : '' }}
                 >
                     DB
                 </button>
                 <button
                     type="button"
                     onClick={() => changeColor('5')}
-                    style={{ backgroundColor: selectedValues.includes('5') ? '#ABE9FF' : '' }}
+                    style={{ backgroundColor: position.includes('5') ? '#ABE9FF' : '' }}
                 >
                     UI / UX
                 </button>
 
-                <input type="hidden" name="selectedValue" value={selectedValues.join(',')} />
+                <input type="hidden" name="position" value={position.join(',')} />
 
                 <div className={style.second_block}>
                     <div>
@@ -96,17 +114,17 @@ const ProjectWrite = () => {
                     </div>
                     <div>
                         <p>프로젝트 종료일</p>
-                        <DatePicker selected={startDate} onChange={date => setEndDate(date)} />
+                        <DatePicker selected={endDate} onChange={date => setEndDate(date)} />
 
                     </div>
                     <div className={style.frame_work_container}>
                         <div>
                             <p>프론트</p>
                             <select name='framework_front'>
-                                <option value={1}>React</option>
-                                <option value={2}>Next.js</option>
-                                <option value={3}>Vue.js</option>
-                                <option value={4}>기타</option>
+                                <option>React</option>
+                                <option>Next.js</option>
+                                <option>Vue.js</option>
+                                <option>기타</option>
 
                             </select>
                         </div>
@@ -139,8 +157,8 @@ const ProjectWrite = () => {
                     <div>
                         <p>상태</p>
                         <select name='recruit'>
-                            <option>모집중</option>
-                            <option>모집완료</option>
+                            <option value={0}>모집중</option>
+                            <option value={1}>모집완료</option>
                         </select>
                     </div>
 
