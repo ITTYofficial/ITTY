bd381888e3f5b93f2bb36ec49798674eadc2549b
philosokey-M <wlghd99589@gmail.com>
2023-09-15 19:40:33 +0900

    0915_19:38_김지홍_회원가입 대충 끝(alert -> focus로 바꿔줘야함)

--- client/src/components/Join.jsx
+++ client/src/components/Join.jsx
@@ -42,11 +42,13 @@ const Join = () => {
   
   const messageElement1 = useRef(null);
   const messageElement2 = useRef(null);
+  const messageElement3 = useRef(null);
   
   useEffect(() => {
     // 컴포넌트가 마운트된 후에 messageElement를 설정
     messageElement1.current = document.getElementById('iDmessage'); // message 요소를 찾아서 설정
     messageElement2.current = document.getElementById('pWmessage');
+    messageElement3.current = document.getElementById('pWCheckmessage');
   }, []);
 
   const onIdHandler = (e) => {
@@ -143,12 +145,12 @@ const idCheck = async(e) => {
 const pwCheck = (e) =>{
   if (pwCheckResult) {
     if(pw !== checkPw){
-      messageElement2.current.textContent = "비밀번호가 일치하지 않습니다.";
-      messageElement2.current.style.color = "red";
+      messageElement3.current.textContent = "비밀번호가 일치하지 않습니다.";
+      messageElement3.current.style.color = "red";
       setPwCheckResult(false);
     }else{
-      messageElement2.current.textContent = "비밀번호 일치 확인";
-      messageElement2.current.style.color = "blue";
+      messageElement3.current.textContent = "비밀번호 일치 확인";
+      messageElement3.current.style.color = "blue";
       setPwCheckResult(true);
     }
   }
@@ -159,34 +161,35 @@ const pwCheck = (e) =>{
 
   const joinMember = async (e) => {
     e.preventDefault();
-if (pw !== checkPw) {
+if (idCheckResult && pwCheckResult && name && nickname&& gender) {
 
-  return alert("비밀번호가 일치하지 않습니다..");
-}
-let member = {
-  id: id,
-  name: name,
-  pw: pw,
-  gender: gender,
-  nickname: nickname,
-  role: role,
-  skill: skill,
-};
-    try {
-      console.log('제발 들어와주라', member);
-      const response = await axios.post("http://localhost:8088/member/join", member); // 경로 테스트 중...
-      if (response.data.message === "회원가입이 완료되었습니다.") {
-        // 성공적으로 삽입되면 리다이렉트 또는 다른 작업 수행
-        window.location.href = '/login'
-        
-        
-      } else {
-        // 오류 처리
-        console.error("회원가입에 실패했습니다.");
+  let member = {
+    id: id,
+    name: name,
+    pw: pw,
+    gender: gender,
+    nickname: nickname,
+    role: role,
+    skill: skill,
+  };
+      try {
+        console.log('제발 들어와주라', member);
+        const response = await axios.post("http://localhost:8088/member/join", member); // 경로 테스트 중...
+        if (response.data.message === "회원가입이 완료되었습니다.") {
+          // 성공적으로 삽입되면 리다이렉트 또는 다른 작업 수행
+          window.location.href = '/login'
+          
+          
+        } else {
+          // 오류 처리
+          console.error("회원가입에 실패했습니다.");
+        }
+      } catch (error) {
+        console.error("오류 발생:", error);
       }
-    } catch (error) {
-      console.error("오류 발생:", error);
-    }
+}else{
+  return alert("다시");
+}
 
   };
 
@@ -221,11 +224,12 @@ let member = {
             <div className="mb-3">
               <label className="form-label" htmlFor="pw">비밀번호</label>
               <input className="form-control" type="password" name="pw" id="pw" value={pw} onChange={onPwHandler} onInput={engNumPwCheck}  placeholder='비밀번호를 입력해주세요(8자리 이상)' />
+              <div id="pWmessage"></div>
             </div>
             <div className="mb-3">
               <label className="form-label" htmlFor="pw_check">비밀번호 확인</label>
               <input className="form-control" type="password" name="pw_check" id="pw_check" value={checkPw} onChange={onCheckPwHandler} onBlur={pwCheck} placeholder='비밀번호를 한번 더 입력해주세요.' />
-              <div id="pWmessage"></div>
+              <div id="pWCheckmessage"></div>
             </div>
             <div className="mb-3">
               <label className="form-label" htmlFor="name">이름</label>
