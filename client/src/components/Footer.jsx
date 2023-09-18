d44176cdf4c4e51bc1539d4e56c649a725a8d93e
BaikJinHyeock <me335097@gmail.com>
2023-09-18 19:00:59 +0900

    0918_19:00_백진혁_자게, 프로젝트 디테일 화면 정상화

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,14 +6,26 @@ const Footer = () => {
     <div className={footer.Wrap_footer}>
       <div>
         <div className={footer.Wrap_footer_left}>
-          <img src="https://i.ibb.co/tshV4tg/image.png" alt="FOOTER_LOGO" />
-        </div>
-        <div className={footer.Wrap_footer_right}>
           <div>
-            <ul>
-              <li></li>
-            </ul>
+            <h4>ITTY</h4>
+            <p>개발자들을 위한 정보공유 </p>
           </div>
+          <div>
+            <div>
+              <h4>팀</h4>
+              <ul>
+                <li>최수환</li>
+                <li>류상지</li>
+                <li>김지홍</li>
+                <li>허광영</li>
+                <li>백진혁</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className={footer.Wrap_footer_right}>
+          <img src="https://i.ibb.co/tshV4tg/image.png" alt="FOOTER_LOGO" />
         </div>
       </div>
     </div>
