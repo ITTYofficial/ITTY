ce59d0007db8ccad799adbc105d333321f0e5f12
최수환 <frcp9408@gmail.com>
2023-09-20 14:49:42 +0900

    0920_14:49_최수환_마이페이지 추가수정

--- client/src/components/MyPage.jsx
+++ client/src/components/MyPage.jsx
@@ -94,15 +94,15 @@ const MyPage = () => {
 
                     {/* 프사 부분 */}
                     <div className={styles.input_pic}>
-                        <div
-                            className={styles.fake_upload}
-                            onClick={handleFakeUploadClick}
-                        >
+                        <div className={styles.fake_upload}>
+                            <Image src='https://i.ibb.co/XsypSbQ/profile-01.png' alt='프로필 미리보기' roundedCircle />
+                        </div>
+                        <div className={styles.img_uploads_btn}
+                            onClick={handleFakeUploadClick}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-camera" viewBox="0 0 16 16">
                                 <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z" />
                                 <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zm0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zM3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z" />
                             </svg>
-                            <span>이미지 등록</span>
                         </div>
                         <input
                             type="file"
@@ -128,7 +128,15 @@ const MyPage = () => {
                 <h4>학원생 인증</h4>
                 <p>ITTY의 특정 서비스를 이용하기 위해서는 스마트인재개발원 소속 인증이 필요합니다.</p>
                 <form>
-                    <h4>소속 인증하기</h4>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shield-fill-exclamation" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm-.55 8.502L7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0zM8.002 12a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+                        </svg>
+                        <div>
+                            <h4>소속 인증하기</h4>
+                            <p>수강 중인 강의실 본인 컴퓨터 상단의 부착된 이름표를 찍어서 첨부파일에 업로드해 주시면 담당자가 본인 여부를 확인 후 학원생으로 전환해 드립니다.</p>
+                        </div>
+                    </div>
                     <input type="file" className="form-control" />
                 </form>
             </div>
