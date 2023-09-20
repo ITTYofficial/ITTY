f2cef680d31ac69a235c13ebe804133f6a691055
philosokey-M <wlghd99589@gmail.com>
2023-09-21 00:55:05 +0900

    0921_00:54_김지홍_회원 테이블 수정 및 로그인 세션문제 해결(미완)

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ import Login from "./components/Login";
 import QuillTest from "./components/QuillTest";
 import PlayBoardWrite from "./components/PlayBoardWrite";
 import { PlayBoardContext } from "./context/PlayBoardContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TestPage from "./components/TestPage";
 import ProjectWrite from "./components/ProjectWrite";
 import MarketWrite from "./components/MarketWrite";
@@ -48,6 +48,36 @@ function App() {
   // playBoardContext로 게시판 작성 데이터를 전역적 사용하기 위한, 작성된 글 데이터를 담을 state
   // 나중에 게시판 별로 새로 만들든, 통합하든해서 사용 할 필요가 있음
   const [value, setValue] = useState("");
+
+  // 새창을 열었을 로그인이 풀리는 문제 해결하기위한 코드
+
+    useEffect(() => {
+      // 컴포넌트가 마운트될 때 실행할 코드 작성
+      // 새창열렸을 때 세션스토리지->로컬스토리지->이동->세션스토리지로 변경
+      window.onstorage = (event) => {
+        if (!event.newValue) return;
+        if (event.key === 'protect-temp' && sessionStorage.getItem('memberId') && sessionStorage.getItem('nickname')) {
+          // 로컬 스토리지에 데이터를 저장한 후 바로 삭제
+          localStorage.setItem('protect-memberId', sessionStorage.getItem('memberId'));
+          localStorage.setItem('protect-nickname', sessionStorage.getItem('nickname'));
+          localStorage.removeItem('protect-memberId');
+          localStorage.removeItem('protect-nickname');
+        } else if ((event.key === 'protect-memberId' || event.key === 'protect-nickname') && (!sessionStorage.getItem('memberId') || !sessionStorage.getItem('nickname'))) {
+          // 로컬 스토리지에 저장한 데이터를 세션 스토리지에 저장
+          const memberId = event.key === 'protect-memberId' ? event.newValue : sessionStorage.getItem('memberId');
+          const nickname = event.key === 'protect-nickname' ? event.newValue : sessionStorage.getItem('nickname');
+          sessionStorage.setItem('memberId', memberId);
+          sessionStorage.setItem('nickname', nickname);
+        }
+      };
+      
+      // 세션 스토리지에 데이터가 없을 경우 로컬 스토리지에서 데이터 가져오기
+      if (!sessionStorage.getItem('memberId') || !sessionStorage.getItem('nickname')) {
+        localStorage.setItem('protect-temp', '1');
+        localStorage.removeItem('protect-temp');
+      }
+    }, []);
+
 
   return (
     <div className="Container">
