00ebe70ed3f5898801c05b45d8fd313a0199e9b5
BaikJinHyeock <me335097@gmail.com>
2023-09-21 12:46:51 +0900

    0921_12:46_백진혁_햄버거 작업중 커밋

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -125,7 +125,6 @@ const Header = () => {
             </ul>
           </li>
         </ul>
-        <button className={Nav.Category_mobile}>&#128100;</button>
       </div>
       <div className={Nav.Member}>
         <ul>
@@ -144,9 +143,24 @@ const Header = () => {
             )}
           </li>
         </ul>
+        <button className={Nav.hamburger_content}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-list"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
+        </button>
         <button className={Nav.Member_mobile}>&#128100;</button>
       </div>
-      <div className={Nav.hamburger_content}>
+      {/* <div className={Nav.hamburger_content}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
@@ -160,7 +174,7 @@ const Header = () => {
             d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
           />
         </svg>
-      </div>
+      </div> */}
     </div>
   );
 };
