e2c47902c06ac9ddf9587bec06edf8c792a4d859
BaikJinHyeock <me335097@gmail.com>
2023-09-27 12:37:05 +0900

    0927_12:36_백진혁_디테일 CSS 수정

--- client/src/components/CommentItem.jsx
+++ client/src/components/CommentItem.jsx
@@ -1,13 +1,12 @@
-import axios from 'axios';
-import React, { useContext, useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
 import PlayBoard from "../css/PlayBoardDetail.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Image from "react-bootstrap/Image";
-import { QuillContext } from '../context/QuillContext';
+import { QuillContext } from "../context/QuillContext";
 
 const CommentItem = ({ props, postId }) => {
-
-    /* 댓글 컴포넌트 작업하러 오신 분에게 남기는 말
+  /* 댓글 컴포넌트 작업하러 오신 분에게 남기는 말
 
     프론트 작업자에게
     PlayBoardDetail에서 떼어온 CommentItem 컴포넌트를 사용해서 이전 작업을 진행 했기때문에
@@ -31,158 +30,185 @@ const CommentItem = ({ props, postId }) => {
     -허허-
     */
 
-    // 댓글 리스트 저장할 State, 댓글 조회, 삭제 함수
-    const { getComment, deleteComment, deleteReComment } = useContext(QuillContext);
+  // 댓글 리스트 저장할 State, 댓글 조회, 삭제 함수
+  const { getComment, deleteComment, deleteReComment } =
+    useContext(QuillContext);
 
-    // 대댓글 작성완료 시 호출되는 함수
-    function reCommentSubmit(event, _id) {
-        event.preventDefault();
-        console.log(_id);
-        const createdAt = new Date().toISOString();
-        const obj = {
-            writer: sessionStorage.getItem("memberNickname"),
-            content: reComment,
-            commentId: _id,
-            createdAt: createdAt
-        };
-        console.log(obj);
-
-        axios.post('http://localhost:8088/comment/reWrite', obj)
-            .then((res) => {
-                alert("댓글이 등록되었습니다.")
-                console.log(res);
-                getComment(postId);
-            })
-            .catch((err) => {
-                console.log(err);
-                alert("게시글 작성 실패")
-            })
-    }
-
-    // 대댓글 내용 담을 State
-    const [reComment, setReComment] = useState();
-
-    // 대댓글 내용 가져오는 함수
-    const reCommentChange = (e) => {
-        setReComment(e.target.value);
-        console.log(e.target.value);
-    }
-
-    // 대댓글 작성 칸 출력 조절 State
-    const [recommentVisible, setRecommentVisible] = useState(false);
-
-    // 대댓글 작성 칸 함수
-    const showRecommentWrite = () => {
-        setRecommentVisible(!recommentVisible);
-    }
-
-    // 날짜를 "몇 시간 전" 형식으로 변환하는 함수
-    const getTime = (dateString) => {
-        const createdAt = new Date(dateString);
-        const now = new Date();
-        const timeDifference = now - createdAt;
-        const hoursDifference = Math.floor(timeDifference / (1000 * 60 * 60));
-        const daysDifference = Math.floor(hoursDifference / 24);
-
-        if (daysDifference === 0) {
-            if (hoursDifference === 0) {
-                return "방금 전";
-            } else {
-                return `${hoursDifference}시간 전`;
-            }
-        } else {
-            return `${daysDifference}일 전`;
-        }
+  // 대댓글 작성완료 시 호출되는 함수
+  function reCommentSubmit(event, _id) {
+    event.preventDefault();
+    console.log(_id);
+    const createdAt = new Date().toISOString();
+    const obj = {
+      writer: sessionStorage.getItem("memberNickname"),
+      content: reComment,
+      commentId: _id,
+      createdAt: createdAt,
     };
+    console.log(obj);
 
-    // 대댓글 컴포넌트
-    const ReComment = ({ commentId, props, index }) => {
+    axios
+      .post("http://localhost:8088/comment/reWrite", obj)
+      .then((res) => {
+        alert("댓글이 등록되었습니다.");
+        console.log(res);
+        getComment(postId);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("게시글 작성 실패");
+      });
+  }
 
-        return (
-            <div className={PlayBoard.recomment_list_box}>
-                <div className={PlayBoard.play_recomment_profile}>
-                    <span>
-                        <Image src={props.writerInfo.profileImg} roundedCircle />
-                    </span>
-                    <span>
-                        <p>{props.writerInfo.class}</p>
-                        <h4>{props.writer}</h4>
-                    </span>
-                    <div className={PlayBoard.recomment_cancel}>
-                        <svg onClick={() => deleteReComment(commentId, postId, index)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
-                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
-                        </svg>
-                    </div>
+  // 대댓글 내용 담을 State
+  const [reComment, setReComment] = useState();
 
-                </div>
-                {/* ===== 댓글 내용이 들어갈 부분 시작 ===== */}
-                <div>
-                    <p>
-                        {props.content}
-                    </p>
-                </div>
-                {/* ===== 댓글 내용이 들어갈 부분 끝 ===== */}
+  // 대댓글 내용 가져오는 함수
+  const reCommentChange = (e) => {
+    setReComment(e.target.value);
+    console.log(e.target.value);
+  };
 
-                <div className={PlayBoard.comment_time_box_2}>
-                    <p>{getTime(props.createdAt)}</p>
-                </div>
+  // 대댓글 작성 칸 출력 조절 State
+  const [recommentVisible, setRecommentVisible] = useState(false);
 
-            </div>
-        )
-    };
+  // 대댓글 작성 칸 함수
+  const showRecommentWrite = () => {
+    setRecommentVisible(!recommentVisible);
+  };
 
+  // 날짜를 "몇 시간 전" 형식으로 변환하는 함수
+  const getTime = (dateString) => {
+    const createdAt = new Date(dateString);
+    const now = new Date();
+    const timeDifference = now - createdAt;
+    const hoursDifference = Math.floor(timeDifference / (1000 * 60 * 60));
+    const daysDifference = Math.floor(hoursDifference / 24);
+
+    if (daysDifference === 0) {
+      if (hoursDifference === 0) {
+        return "방금 전";
+      } else {
+        return `${hoursDifference}시간 전`;
+      }
+    } else {
+      return `${daysDifference}일 전`;
+    }
+  };
+
+  // 대댓글 컴포넌트
+  const ReComment = ({ commentId, props, index }) => {
     return (
-        <div className={PlayBoard.comment_list}>
-            <div className={PlayBoard.play_comment_profile}>
-                <span>
-                    <Image src={props.writerInfo.profileImg} roundedCircle />
-                </span>
-                <span>
-                    <p>{props.writerInfo.class}</p>
-                    <h4>{props.writer}</h4>
-                    <div className={PlayBoard.comment_cancel}>
-                        <svg onClick={() => deleteComment(props._id, postId)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
-                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
-                        </svg>
-                    </div>
-                </span>
-            </div>
-            {/* ===== 댓글 내용이 들어갈 부분 시작 ===== */}
-            <div>
-
-                <p>
-                    {props.content}
-                </p>
-
-            </div>
-            {/* ===== 댓글 내용이 들어갈 부분 끝 ===== */}
-
-            <div>
-                <p className={PlayBoard.comment_time_box}>{getTime(props.createdAt)}</p>
-            </div>
-            <div className={PlayBoard.recomment_button_box} onClick={showRecommentWrite}>
-                <span className={PlayBoard.recomment_button_box_2}>댓글쓰기</span>
-            </div>
-
-            {recommentVisible &&
-                <form onSubmit={(event) => reCommentSubmit(event, props._id)}>
-                    <div className={PlayBoard.recomment_write}>
-                        <div>
-                            <div>
-                                <img src="#" />
-                            </div>``
-                            <textarea onBlur={reCommentChange} placeholder="댓글을 쓰려면 로그인이 필요합니다."></textarea>
-                        </div>
-                        <button type="submit">댓글쓰기</button>
-                    </div>
-                </form>
-            }
-            {props.reComment.map((item, index) => <ReComment key={index} commentId={props._id} props={item} index={index} />)}
-
+      <div className={PlayBoard.recomment_list_box}>
+        <div className={PlayBoard.play_recomment_profile}>
+          <span>
+            <Image src={props.writerInfo.profileImg} roundedCircle />
+          </span>
+          <span>
+            <p>{props.writerInfo.class}</p>
+            <h4>{props.writer}</h4>
+          </span>
+          <div className={PlayBoard.recomment_cancel}>
+            <svg
+              onClick={() => deleteReComment(commentId, postId, index)}
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-trash"
+              viewBox="0 0 16 16"
+            >
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
+              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+            </svg>
+          </div>
         </div>
-    );
-}
+        {/* ===== 댓글 내용이 들어갈 부분 시작 ===== */}
+        <div>
+          <p>{props.content}</p>
+        </div>
+        {/* ===== 댓글 내용이 들어갈 부분 끝 ===== */}
 
-export default CommentItem
+        <div className={PlayBoard.comment_time_box_2}>
+          <p>{getTime(props.createdAt)}</p>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className={PlayBoard.comment_list}>
+      <div className={PlayBoard.play_comment_profile}>
+        <span>
+          <Image src={props.writerInfo.profileImg} roundedCircle />
+        </span>
+        <span>
+          <p>{props.writerInfo.class}</p>
+          <h4>{props.writer}</h4>
+          <div className={PlayBoard.comment_cancel}>
+            <svg
+              onClick={() => deleteComment(props._id, postId)}
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-trash"
+              viewBox="0 0 16 16"
+            >
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
+              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+            </svg>
+          </div>
+        </span>
+      </div>
+      {/* ===== 댓글 내용이 들어갈 부분 시작 ===== */}
+      <div>
+        <p>{props.content}</p>
+      </div>
+      {/* ===== 댓글 내용이 들어갈 부분 끝 ===== */}
+
+      <div>
+        <p className={PlayBoard.comment_time_box}>{getTime(props.createdAt)}</p>
+        <span
+          className={PlayBoard.recomment_button_box_2}
+          onClick={showRecommentWrite}
+        >
+          댓글쓰기
+        </span>
+      </div>
+      {/* <div
+        className={PlayBoard.recomment_button_box}
+        
+      >
+        
+      </div> */}
+
+      {recommentVisible && (
+        <form onSubmit={(event) => reCommentSubmit(event, props._id)}>
+          <div className={PlayBoard.recomment_write}>
+            <div>
+              <div>
+                <img src="#" />
+              </div>
+              <textarea
+                onBlur={reCommentChange}
+                placeholder="댓글을 쓰려면 로그인이 필요합니다."
+              ></textarea>
+            </div>
+            <button type="submit">댓글쓰기</button>
+          </div>
+        </form>
+      )}
+      {props.reComment.map((item, index) => (
+        <ReComment
+          key={index}
+          commentId={props._id}
+          props={item}
+          index={index}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CommentItem;
--- client/src/components/QnaDetail.jsx
+++ client/src/components/QnaDetail.jsx
@@ -29,7 +29,6 @@ const QnaDetail = () => {
     <span className={`${style.play_title} ${style.life}`}>생활/기타🌷</span>
   );
 
-
   // 게시글정보 저장할 State
   const [qnaDetail, setQnADetail] = useState([]);
   const [visible, setVisible] = useState([false, false, false, false]);
@@ -54,22 +53,23 @@ const QnaDetail = () => {
   // 특정 게시글의 작성자 정보를 조회하기 위한 nickname값 가져오기-지홍
   const location = useLocation();
   const params = new URLSearchParams(location.search);
-  const nickname = params.get('id');
+  const nickname = params.get("id");
 
   // 회원정보 저장할 state-지홍
   const [memberInfo, setMemberInfo] = useState([]);
 
   // 회원 정보 조회 함수
   const memberSearching = async () => {
-    await axios.get(`http://localhost:8088/member/memberSearching?id=${nickname}`)
+    await axios
+      .get(`http://localhost:8088/member/memberSearching?id=${nickname}`)
       .then((res) => {
-        console.log('axios다음 니크네임', res.data.member.nickname);
+        console.log("axios다음 니크네임", res.data.member.nickname);
         setMemberInfo(res.data.member);
       })
       .catch((err) => {
-        console.log('err :', err);
-      })
-  }
+        console.log("err :", err);
+      });
+  };
 
   // 댓글 내용 담을 State
   const [comment, setComment] = useState();
@@ -80,7 +80,7 @@ const QnaDetail = () => {
   // 댓글 내용 가져오는 함수
   const commentChange = (e) => {
     setComment(e.target.value);
-  }
+  };
 
   // 댓글 작성완료 시 호출되는 함수
   function commentSubmit(event) {
@@ -88,29 +88,29 @@ const QnaDetail = () => {
     const obj = {
       writer: sessionStorage.getItem("memberNickname"),
       postid: id,
-      content: comment
+      content: comment,
     };
     console.log(obj);
 
-    axios.post('http://localhost:8088/comment/write', obj)
+    axios
+      .post("http://localhost:8088/comment/write", obj)
       .then((res) => {
-        alert("댓글이 등록되었습니다.")
+        alert("댓글이 등록되었습니다.");
         console.log(res);
         getComment(id);
       })
       .catch((err) => {
         console.log(err);
-        alert("게시글 작성 실패")
-      })
-  };
+        alert("게시글 작성 실패");
+      });
+  }
 
   // 페이지 빠져나갈 때 댓글 리스트 초기화
   useEffect(() => {
     return () => {
       setCommentList([]);
-    }
-  }, [])
-
+    };
+  }, []);
 
   // 페이지 렌더링시 조회함수 실행
   useEffect(() => {
@@ -230,7 +230,7 @@ const QnaDetail = () => {
                 </span>
               </span>
             </div>
-            <hr className={style.division_line_2}></hr>
+            <hr />
           </div>
           {/* 자유게시판 상세페이지 상단 제목부분 Finish */}
 
@@ -254,7 +254,7 @@ const QnaDetail = () => {
                 {meat && <Dropdown />}
               </ul>
             </div>
-            <div className='quill_content_font_style'>
+            <div className="quill_content_font_style">
               <span
                 dangerouslySetInnerHTML={{ __html: qnaDetail.content }}
               ></span>
@@ -274,17 +274,22 @@ const QnaDetail = () => {
                 <div>
                   <img src="#" />
                 </div>
-                <textarea onBlur={commentChange} placeholder="댓글을 쓰려면 로그인이 필요합니다."></textarea>
+                <textarea
+                  onBlur={commentChange}
+                  placeholder="댓글을 쓰려면 로그인이 필요합니다."
+                ></textarea>
               </div>
               <button type="submit">댓글쓰기</button>
             </div>
           </form>
           {/* 댓글달기 끝 */}
 
-          {commentList.map((item) => (<QnaCommentItem key={item._id} props={item} postId={id} />))}
+          {commentList.map((item) => (
+            <QnaCommentItem key={item._id} props={item} postId={id} />
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
 export default QnaDetail;
